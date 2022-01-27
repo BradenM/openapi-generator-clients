761a0785520b0c8d76752b3cacd15fdcf6fc0229
Braden Mars <bradenmars@bradenmars.me>
2022-01-27 05:08:45 -0600

    feat(clients): add build config for server/v1

--- build.config.ts
+++ build.config.ts
@@ -1,7 +1,16 @@
 import { defineBuildConfig } from 'unbuild'
 import pkg from './package.json'
+
 export default defineBuildConfig({
-  entries: ['src/index'],
+  entries: [
+    {
+      name: 'server/v1',
+      outDir: './dist/server/v1',
+      format: 'esm',
+      input: './server/v1/index',
+      declaration: true
+    }
+  ],
   declaration: true,
   externals: [...Object.keys(pkg.dependencies)]
 })
