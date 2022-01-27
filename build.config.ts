7755f6f060f7484a7361293d5e47758f7ed417c3
Braden Mars <bradenmars@bradenmars.me>
2022-01-27 05:39:25 -0600

    feat(clients): add clever/server root + versioned build entries

--- build.config.ts
+++ build.config.ts
@@ -4,10 +4,31 @@ import pkg from './package.json'
 export default defineBuildConfig({
   entries: [
     {
-      name: 'server/v1',
+      name: 'server/v1/index',
       outDir: './dist/server/v1',
       format: 'esm',
       input: './server/v1/index',
+      declaration: true
+    },
+    {
+      name: 'server/index',
+      outDir: './dist/server',
+      format: 'esm',
+      input: './server/v1/index',
+      declaration: true
+    },
+    {
+      name: 'clever/v1/index',
+      outDir: './dist/clever/v1',
+      format: 'esm',
+      input: './clever/v1/index',
+      declaration: true
+    },
+    {
+      name: 'clever/index',
+      outDir: './dist/clever',
+      format: 'esm',
+      input: './clever/v1/index',
       declaration: true
     }
   ],
