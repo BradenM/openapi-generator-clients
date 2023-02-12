48209ceae41887a1cf66e61e2b4f1b4f572d5639
Braden Mars <bradenmars@bradenmars.me>
2023-02-12 04:35:19 -0600

    feat(example): add pet-store clients example.
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

---
+++ example/clients-example/clients.config.ts
@@ -0,0 +1,21 @@
+import {defineClients} from 'openapi-generator-clients'
+import tsGenerator from '@openapi-generator-clients/ts-generator'
+
+const petstore = tsGenerator.defineClient('v1', {
+	generators: {
+		'petstore-v1': {
+			inputSpec:
+				'https://raw.githubusercontent.com/OpenAPITools/openapi-petstore/master/src/main/resources/openapi.yaml',
+			output: 'clients/petstore/v1',
+			gitHost: 'github.com',
+			gitRepoId: 'openapi-generator-clients',
+			gitUserId: 'BradenM',
+			legacyDiscriminatorBehavior: false,
+			removeOperationIdPrefix: true,
+			additionalProperties: {},
+			additionalArgs: [],
+		},
+	},
+})
+
+export default defineClients(petstore)
