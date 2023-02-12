73292b4d37f8f7d1e325bedfb3e7bcf5c32dbc68
Braden Mars <bradenmars@bradenmars.me>
2023-02-12 03:41:43 -0600

    test(core): add tests for build client.
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

---
+++ packages/openapi-generator-clients/test/build.test.ts
@@ -0,0 +1,108 @@
+import {vi, it, describe, expect, type SpyInstance, beforeEach} from 'vitest'
+import type {
+	Workspace,
+	WorkspaceProvider,
+} from '@openapi-generator-clients/types'
+import {
+	ClientRecord,
+	GeneratorRecord,
+	TemplateConfigRecord,
+} from '@openapi-generator-clients/config'
+import {buildClient} from 'openapi-generator-clients'
+
+interface Context {
+	workspaceProvider: WorkspaceProvider
+	providerSpy: {
+		create: SpyInstance
+		addTemplatePath: SpyInstance
+		setConfig: SpyInstance
+	}
+	inClient: ClientRecord
+	workClient: ClientRecord
+}
+
+describe('buildClient', () => {
+	const testWorkspace: Workspace = {
+		rootPath: 'some-root',
+		outputPath: 'test-output',
+		configPath: 'test.config.json',
+		templatesPath: 'test-templates',
+	}
+
+	const itx = it<Context>
+
+	beforeEach<Context>(async (ctx) => {
+		ctx.workspaceProvider = {
+			workspace: {...testWorkspace},
+			async create() {
+				return this
+			},
+			async addTemplatePath() {
+				return this
+			},
+			async setConfig() {
+				return this
+			},
+		}
+		ctx.providerSpy = {
+			create: vi.spyOn(ctx.workspaceProvider, 'create'),
+			addTemplatePath: vi.spyOn(ctx.workspaceProvider, 'addTemplatePath'),
+			setConfig: vi.spyOn(ctx.workspaceProvider, 'setConfig'),
+		}
+		ctx.inClient = new ClientRecord({
+			name: 'test',
+			generator: GeneratorRecord().mergeDeep({
+				generatorVersion: 'v0',
+				generatorName: 'test-generator',
+				output: 'test-output',
+			}),
+			templateVersion: {
+				version: 'v0',
+				path: 'test-generator',
+				config: TemplateConfigRecord().mergeDeep({generatorVersion: 'v0'}),
+			},
+			templateConfig: TemplateConfigRecord().merge({generatorVersion: 'v0'}),
+			template: {name: 'test-template', root: 'path'},
+		})
+		ctx.workClient = await buildClient(ctx.inClient, {
+			clients: [],
+			overwrite: false,
+			rootDir: '.',
+			additionalArgs: [],
+			workspaceProvider: ctx.workspaceProvider,
+		})
+	})
+
+	itx('creates expected workspace.', async (ctx) => {
+		expect(ctx.workClient.toJS()).toMatchObject({
+			workspace: testWorkspace,
+		})
+	})
+
+	itx('creates expected work client.', async (ctx) => {
+		expect(ctx.workClient.toJS()).toMatchSnapshot('work client')
+	})
+
+	itx('creates expected oas config.', async (ctx) => {
+		expect(ctx.workClient.oasConfig.toJS()).toMatchSnapshot('oas config')
+	})
+
+	itx('creates workspace and adds template paths.', async (ctx) => {
+		expect(ctx.providerSpy.create).toHaveBeenCalledTimes(1)
+		expect(ctx.providerSpy.addTemplatePath).toHaveBeenCalledTimes(2)
+		expect(ctx.providerSpy.create.mock.lastCall).toStrictEqual([
+			'test-output',
+			false,
+		])
+		expect(ctx.providerSpy.addTemplatePath.mock.lastCall).toEqual([
+			'test-generator',
+		])
+	})
+
+	itx('writes expected oas config to workspace config.', async (ctx) => {
+		expect(ctx.providerSpy.setConfig).toHaveBeenCalledTimes(1)
+		expect(ctx.providerSpy.setConfig.mock.calls).toMatchSnapshot(
+			'workspace oas config.',
+		)
+	})
+})
