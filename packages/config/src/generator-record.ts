d135123eaa298fd8f91b184a810ee8f9f6d30410
Braden Mars <bradenmars@bradenmars.me>
2023-02-12 03:24:30 -0600

    feat(config): add generator-record interfaces / records.
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

---
+++ packages/config/src/generator-record.ts
@@ -0,0 +1,73 @@
+import type {
+	ExtendedGeneratorConfig,
+	GeneratorConfig,
+	OASConfig,
+	TemplateConfig,
+} from '@openapi-generator-clients/types'
+import {Record as ImRecord, type RecordOf} from 'immutable'
+import type {PartialDeep} from 'type-fest'
+
+const OAS_GENERATOR_VERSION = '6.0.0-beta'
+
+export const templateConfigDefaults: TemplateConfig = {
+	generatorVersion: '',
+	drop: [],
+	additionalArgs: [],
+}
+
+export const generatorRecordDefaults: GeneratorConfig = {
+	gitRepoId: '',
+	gitUserId: '',
+	gitHost: 'github.com',
+	generatorName: '',
+	templateDir: '',
+	output: '',
+	inputSpec: undefined,
+	glob: undefined,
+	removeOperationIdPrefix: true,
+	legacyDiscriminatorBehavior: false,
+	additionalProperties: {
+		supportsES6: true,
+		usePromise: true,
+		useRxJS6: true,
+		withInterfaces: true,
+		useInversify: undefined,
+		useObjectParameters: undefined,
+		legacyDiscriminatorBehavior: false,
+		platform: undefined,
+		disallowAdditionalPropertiesIfNotPresent: undefined,
+		npmName: undefined,
+	},
+}
+
+export const GeneratorRecord = ImRecord<ExtendedGeneratorConfig>(
+	{
+		...templateConfigDefaults,
+		...generatorRecordDefaults,
+	},
+	'GeneratorRecord',
+)
+
+export const TemplateConfigRecord = ImRecord<TemplateConfig>(
+	templateConfigDefaults,
+	'TemplateConfigRecord',
+)
+
+export const OASGeneratorRecord = ImRecord<GeneratorConfig>(
+	generatorRecordDefaults,
+	'OASGeneratorRecord',
+)
+export interface OASGeneratorRecord extends RecordOf<GeneratorConfig> {}
+
+export const OASRecord: ImRecord.Factory<PartialDeep<OASConfig>> = ImRecord<
+	PartialDeep<OASConfig>
+>(
+	{
+		$schema:
+			'node_modules/@openapitools/openapi-generator-cli/config.schema.json',
+		spaces: 2,
+		'generator-cli': {version: OAS_GENERATOR_VERSION, generators: {}},
+	},
+	'OASRecord',
+)
+export interface OASRecord extends RecordOf<OASConfig> {}
